b0f3faa5ba54a3b401bdfc2d23762d970b43a7cf
Leon Edlund <Leone.edlund@gmail.com>
2024-10-11 00:10:36 +0200

    initializing styling

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en" data-theme="light">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="color-scheme" content="light" />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.conditional.min.css">
+  <link rel="stylesheet" href="styles/reset.css">
+  <link rel="stylesheet" href="styles/style.css">
+  <title>Hello world!</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <h1>Anslagstavla</h1>
+      <div class="pico">
+        <button>Ladda upp</button>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+    <aside>
+      <nav>
+        <ul>
+          <li><a href="" id="active">Alla annonser</a></li>
+          <li><a href="">Evenemang</a></li>
+          <li><a href="">Aktiviteter</a></li>
+          <li><a href="">Utbildning och Kurser</a></li>
+          <li><a href="">Köp/Sälj/Byt</a></li>
+          <li><a href="">Efterlyst/Borttappat</a></li>
+          <li><a href="">Samhällsinformation</a></li>
+        </ul>
+      </nav>
+
+
+    </aside>
+
+    <section>
+      <h2>Alla Annonser</h2>
+      <div id="post-grid">
+        <a href="" class="pico">
+          <article>
+            <header>
+              <strong>Borttappad hund</strong>
+            </header>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab perspiciatis odit repellat, ut nesciunt deserunt atque animi dolores error placeat alias molestiae praesentium, soluta recusandae ipsa aliquam minima natus labore!</p>
+            <footer>20-12-2024</footer>
+          </article>
+        </a>
+
+      </div>
+
+    </section>
+  </main>
+</body>
+
+</html>
